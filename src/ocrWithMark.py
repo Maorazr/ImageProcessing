2d4284c1a4b71999a8f012467c9b3657cac6577b
Maorazr <maoraz@post.bgu.ac.il>
2023-08-01 17:07:17 +0300

    annotation

--- src/ocrWithMark.py
+++ src/ocrWithMark.py
@@ -1,7 +1,39 @@
 import cv2
 import numpy as np
 from sklearn.cluster import DBSCAN
-import pytesseract
+
+# Initialize the list of rectangles and boolean indicating
+# whether cropping is being performed or not
+rectangles = []
+current_rectangle = []
+
+
+def find_rectangle(point, rectangles):
+    x, y = point
+    for rect in rectangles:
+        x_min, y_min, x_max, y_max = rect
+        if x_min <= x <= x_max and y_min <= y <= y_max:
+            return rect
+    return None
+
+
+def click_and_crop(event, x, y, flags, param):
+    # grab references to the global variables
+    global rectangles, current_rectangle
+
+    # if the left mouse button was clicked, record the starting
+    # (x, y) coordinates and indicate that cropping is being
+    # performed
+    if event == cv2.EVENT_LBUTTONDOWN:
+        current_rectangle = [(x, y)]
+        rect = find_rectangle((x, y), rectangles)
+
+        # check if the point is within a rectangle
+        if rect is not None:
+            # draw a rectangle around the region of interest
+            cv2.rectangle(image, (rect[0], rect[1]), (rect[2], rect[3]), (0, 255, 0), 2)
+            cv2.imshow("image", image)
+
 
 # Load the image
 image = cv2.imread('/Users/maorazriel/PycharmProjects/pythonProject4/src/try.jpeg')
@@ -29,14 +61,10 @@ for contour in contours:
 # Convert the list of coordinates to a numpy array
 rects = np.array(rects)
 
-
 # Apply DBSCAN to group the coordinates
 clustering = DBSCAN(eps=120, min_samples=1).fit(rects)
 
-
-
 # Iterate over the clusters and draw bounding boxes around each one
-rectangles = []
 for idx, class_ in enumerate(set(clustering.labels_)):
     if class_ != -1:
         same_group = np.array(rects)[np.where(clustering.labels_ == class_)[0]]
@@ -49,12 +77,17 @@ for idx, class_ in enumerate(set(clustering.labels_)):
         # Add the rectangle to the list
         rectangles.append((x_min, y_min, x_max, y_max))
 
+cv2.namedWindow("image")
+cv2.setMouseCallback("image", click_and_crop)
 
-        #cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
-        #cv2.putText(image, str(idx), (x_min, y_min), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+# Keep the window open until the 'c' key is pressed
+while True:
+    # display the image and wait for a keypress
+    cv2.imshow("image", image)
+    key = cv2.waitKey(1) & 0xFF
 
+    if key == ord("c"):  # if the 'c' key is pressed, break from the loop
+        break
 
-#
-# cv2.imshow('Image with Bounding Boxes', image)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+# close all open windows
+cv2.destroyAllWindows()
